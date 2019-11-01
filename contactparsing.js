a91e79095df595b2669dd2febc828ac9be018ecc
Alexrodriguez2090 <54908770+Alexrodriguez2090@users.noreply.github.com>
2019-11-01 16:54:41 -0500

    Add files via upload

--- contactparsing.js
+++ contactparsing.js
@@ -1,6 +1,6 @@
 var contacts = [];
-
-var contactsID = document.getElementById("contacts");
+var contactsSquared = [];
+var contactProgress = 0;
 
 function getIndex() {
     var indexLoad = new XMLHttpRequest();
@@ -14,16 +14,29 @@ function getIndex() {
     indexLoad.send();
 }
 
-function showContact() {
-	for (i = 0; i < contacts.length; i++) {
-		console.log(contacts[i]);
-		var ofIndexLoad = new XMLHttpRequest();
-    	ofIndexLoad.open("GET", contacts[i]);
+function nextContact() {
+	for (contactProgress = 0; contactProgress < contacts.length; contactProgress++) {
+		showContact()
+	}
+}
 
-    	ofIndexLoad.onload = function() {
-    		console.log(ofIndexLoad.responseText)
-    		contactOfContacts = JSON.parse(ofIndexLoad.responseText);
-		}
-    	ofIndexLoad.send();
+function showContact() {
+	console.log(contacts[contactProgress]);
+	var ofIndexLoad = new XMLHttpRequest();
+    ofIndexLoad.open("GET", contacts[contactProgress]);
+
+    ofIndexLoad.onload = function() {
+    	console.log(ofIndexLoad.responseText)
+    	contactOfContacts = JSON.parse(ofIndexLoad.responseText);
+    	contactsSquared.push(contactOfContacts)
+	}
+    ofIndexLoad.send();
+}
+function check() {
+	for (i = 0; i < contactsSquared.length; i++) {
+		console.log(contactsSquared[i])
+		document.getElementById("contactsID").insertAdjacentHTML("beforeend","<h3>Contact: " + contactsSquared[i].preferredName + "</h3>First Name: " + contactsSquared[i].firstName + 
+			"<br>Last Name: " + contactsSquared[i].lastName + "<br>Email: " + contactsSquared[i].email + "<br>Phone #: " + contactsSquared[i].phoneNumber + 
+			"<br>City: " + contactsSquared[i].city + ", " + contactsSquared[i].state + "<br>Hobby: " + contactsSquared[i].favoriteHobby + "<br><br><br>");
 	}
 }
